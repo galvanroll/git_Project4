315f5c65d5c5c057412b52ff905906ad1a563a04
ruthdgalvan <ruthdgalvan@gmail.com>
2022-01-24 10:38:41 -0500

    changed work commit

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -3,6 +3,7 @@ package hello;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("b25 learning github");
     }
 
 
